9b5be18521cc1ecd1ae402aa974ed598b047d145
Edwar Aguiar <Aguiar.Edwar@gmail.com>
2022-05-30 06:35:24 -0500

    C14 - Se Agrego Persistencia de Datos - Comenzamos a usar localStorage

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './index.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App saludo="Propiedad Props o Parametro">
-    <h1> Propiedad Children </h1>
-  </App>,
+  <App />,
   document.getElementById('root')
 );
